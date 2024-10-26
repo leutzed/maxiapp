127c3b16cde934a222e036656c8045614aaf6058
Renan Dorneles Schuquel <rdorneles64@gmail.com>
2024-10-26 11:39:48 -0300

    feat:Adicionada config Docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:18.20.2-alpine
+RUN mkdir -p /home/node/app
+WORKDIR /home/node/app
+RUN chown -R node:node .
+USER node
+COPY ../../app/package*.json /home/node/app
+RUN npm install --force --loglevel verbose
+COPY ./app/src /home/node/app/src
+CMD [ "node", "./src/index.js" ]
--- app/src/index.js
+++ app/src/index.js
@@ -1,101 +1,100 @@
-setTimeout(() => {
-    console.log('out');
-}, 100000)
-// const express = require('express');
-// const axios = require('axios');
-// const xml2js = require('xml2js');
-// const session = require('express-session'); // Importando express-session
+const express = require('express');
+const axios = require('axios');
+const xml2js = require('xml2js');
+const session = require('express-session');
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 
-// // Configuração das sessões
-// app.use(session({
-//     secret: 'seu-segredo-aqui', // Substitua por uma string secreta
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false } // Defina como true se estiver usando HTTPS
-// }));
+app.use(session({
+    secret: 'seu-segredo-aqui',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-// // Função para converter XML para JSON
-// const parseXML = (xml) => {
-//     return new Promise((resolve, reject) => {
-//         xml2js.parseString(xml, { explicitArray: false }, (err, result) => {
-//             if (err) {
-//                 return reject(err);
-//             }
-//             resolve(result);
-//         });
-//     });
-// };
+const parseXML = (xml) => {
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(xml, { explicitArray: false }, (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(result);
+        });
+    });
+};
 
-// // Rota de login
-// app.post('/api/login', async (req, res) => {
-//   const { user, scode } = req.body;
+app.get('/healthcheck', (_, res) => {
+    console.log('OK');
+    res.status(200).end();
+});
 
-//   try {
-//       const response = await axios.get(`http://www.maxithlon.com/maxi-xml/login.php`, {
-//           params: { user, scode },
-//           withCredentials: true,
-//       });
+app.post('/login', async (req, res) => {
+    const { user, scode } = req.body;
 
-//       const jsonResponse = await parseXML(response.data);
+    if(!user || !scode){
+        return res.status(400).end();
+    }
 
-//       if (jsonResponse['maxi-xml'].error) {
-//           return res.status(401).json({ message: jsonResponse['maxi-xml'].error });
-//       }
+    try {
+        const response = await axios.get(`http://www.maxithlon.com/maxi-xml/login.php`, {
+            params: { user, scode },
+            withCredentials: true,
+        });
 
-//       // Armazenar o ID do usuário e o cookie de sessão na sessão
-//       req.session.userId = user;
-//       const setCookieHeader = response.headers['set-cookie'];
-//       if (setCookieHeader) {
-//           // Armazenar o cookie de sessão na sessão do usuário
-//           req.session.cookieValue = setCookieHeader[0]; // Armazena o primeiro cookie
-//       }
+        const jsonResponse = await parseXML(response.data);
 
-//       return res.json({ message: jsonResponse['maxi-xml'].login });
-//   } catch (error) {
-//       return res.status(500).json({ error: 'Erro ao realizar login' });
-//   }
-// });
+        if (jsonResponse['maxi-xml'].error) {
+            return res.status(401).json({ message: jsonResponse['maxi-xml'].error });
+        }
 
-// // Rota para obter detalhes dos atletas
-// app.get('/api/athletes', async (req, res) => {
-//   const { teamid } = req.query;
+        req.session.userId = user;
+        const setCookieHeader = response.headers['set-cookie'];
+        if (setCookieHeader) {
+            req.session.cookieValue = setCookieHeader[0];
+        }
 
-//   try {
-//       const userId = req.session.userId;
-//       const idToUse = teamid || userId;
-//       const cookieValue = req.session.cookieValue;
+        return res.json({ message: jsonResponse['maxi-xml'].login });
+    } catch (error) {
+        return res.status(500).json({ error: 'Erro ao realizar login' });
+    }
+});
 
-//       if (!cookieValue) {
-//           return res.status(401).json({ message: 'Usuário não autenticado' });
-//       }
+app.get('/api/athletes', async (req, res) => {
+    const { teamid } = req.query;
 
-//       const response = await axios.get(`http://www.maxithlon.com/maxi-xml/athletes.php`, {
-//           params: { teamid: idToUse },
-//           headers: {
-//               Cookie: cookieValue // Enviar o cookie de sessão armazenado
-//           },
-//           withCredentials: true,
-//       });
+    try {
+        const userId = req.session.userId;
+        const idToUse = teamid || userId;
+        const cookieValue = req.session.cookieValue;
 
-//       const jsonResponse = await parseXML(response.data);
+        if (!cookieValue) {
+            return res.status(401).json({ message: 'Usuário não autenticado' });
+        }
 
-//       if (jsonResponse['maxi-xml'].error) {
-//           return res.status(400).json({ message: jsonResponse['maxi-xml'].error });
-//       }
+        const response = await axios.get(`http://www.maxithlon.com/maxi-xml/athletes.php`, {
+            params: { teamid: idToUse },
+            headers: {
+                Cookie: cookieValue
+            },
+            withCredentials: true,
+        });
 
-//       console.log(jsonResponse['maxi-xml'].athlete);
-//       return res.json(jsonResponse['maxi-xml'].athlete);
-//   } catch (error) {
-//       return res.status(500).json({ error: 'Erro ao obter detalhes dos atletas' });
-//   }
-// });
+        const jsonResponse = await parseXML(response.data);
 
-// // Iniciar o servidor
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando na porta ${PORT}`);
-// });
+        if (jsonResponse['maxi-xml'].error) {
+            return res.status(400).json({ message: jsonResponse['maxi-xml'].error });
+        }
+
+        console.log(jsonResponse['maxi-xml'].athlete);
+        return res.json(jsonResponse['maxi-xml'].athlete);
+    } catch (error) {
+        return res.status(500).json({ error: 'Erro ao obter detalhes dos atletas' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
