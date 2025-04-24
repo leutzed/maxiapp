dd969f4c7d9725e8fcfe00cc00697362c8d30446
leutzed <leutzed@gmail.com>
2025-04-24 10:33:11 -0300

    :sparkles: feat: fix docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
+    port: 8080,
     proxy: {
       '/auth': {
         target: 'http://localhost:3000',
